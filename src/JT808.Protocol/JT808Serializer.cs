d475cc35b293edca336f983fc321feec78dc4626
SmallChi <564952747@qq.com>
2018-10-14 22:16:16 +0800

    修改序列化类

--- src/JT808.Protocol/JT808Serializer.cs
+++ src/JT808.Protocol/JT808Serializer.cs
@@ -12,86 +12,41 @@ namespace JT808.Protocol
     /// </summary>
     public static class JT808Serializer
     {
-        public static byte[] Serialize(JT808Package jT808Package)
+        public static byte[] Serialize(JT808Package jT808Package, int minBufferSize = 4096)
         {
-            var formatter = JT808FormatterExtensions.GetFormatter<JT808Package>();
-            var pool = MemoryPool<byte>.Shared;
-            IMemoryOwner<byte> buffer = pool.Rent(4096);
-            try
-            {
-                var len = formatter.Serialize(buffer, 0, jT808Package);
-                return buffer.Memory.Slice(0, len).ToArray();
-            }
-            catch (JT808Exception ex)
-            {
-                throw new JT808Exception("Serialize", ex);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Serialize", ex);
-            }
-            finally
-            {
-                // 源码：System.Memory.MemoryPool 
-                // private static readonly MemoryPool<T> s_shared = new ArrayMemoryPool<T>();
-                // 单例内存池 不需要手动释放资源
-                 buffer.Dispose();
-            }
+            return Serialize<JT808Package>(jT808Package, minBufferSize);
         }
 
         public static JT808Package Deserialize(ReadOnlySpan<byte> bytes)
         {
-            try
-            {
-                var formatter = JT808FormatterExtensions.GetFormatter<JT808Package>();
-                int readSize;
-                return formatter.Deserialize(bytes,out readSize);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Deserialize", ex);
-            }
+            return Deserialize<JT808Package>(bytes);
         }
 
-        public static byte[] Serialize<T>(T obj)
+        public static byte[] Serialize<T>(T obj, int minBufferSize = 4096)
         {
             var formatter = JT808FormatterExtensions.GetFormatter<T>();
             var pool = MemoryPool<byte>.Shared;
-            IMemoryOwner<byte> buffer = pool.Rent(10240);
+            IMemoryOwner<byte> buffer = pool.Rent(minBufferSize);
             try
             {
                 var len = formatter.Serialize(buffer, 0, obj);
                 return buffer.Memory.Slice(0, len).ToArray();
             }
-            catch (JT808Exception ex)
-            {
-                throw new JT808Exception("Serialize", ex);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Serialize", ex);
-            }
             finally
             {
                 // 源码：System.Memory.MemoryPool 
                 // private static readonly MemoryPool<T> s_shared = new ArrayMemoryPool<T>();
                 // 单例内存池 不需要手动释放资源
+                // buffer.Dispose() 相当于调用ArrayPool<T>.Shared.Return(array)
                 buffer.Dispose();
             }
         }
 
         public static T Deserialize<T>(ReadOnlySpan<byte> bytes)
         {
-            try
-            {
-                var formatter = JT808FormatterExtensions.GetFormatter<T>();
-                int readSize;
-                return formatter.Deserialize(bytes,out readSize);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Deserialize", ex);
-            }
+            var formatter = JT808FormatterExtensions.GetFormatter<T>();
+            int readSize;
+            return formatter.Deserialize(bytes,out readSize);
         }
     }
 }
