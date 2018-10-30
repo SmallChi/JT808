451b1518e147035c365459a97cb997ac75b9fe1d
SmallChi <564952747@qq.com>
2018-10-30 19:22:11 +0800

    增加0x0800消息

---
+++ src/JT808.Protocol/JT808Formatters/MessageBodyFormatters/JT808_0x0800Formatter.cs
@@ -0,0 +1,35 @@
+﻿using JT808.Protocol.Extensions;
+using JT808.Protocol.MessageBody;
+using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JT808.Protocol.JT808Formatters.MessageBodyFormatters
+{
+    public class JT808_0x0800Formatter : IJT808Formatter<JT808_0x0800>
+    {
+        public JT808_0x0800 Deserialize(ReadOnlySpan<byte> bytes, out int readSize)
+        {
+            int offset = 0;
+            JT808_0x0800 jT808_0X0800 = new JT808_0x0800();
+            jT808_0X0800.MultiMediaId = JT808BinaryExtensions.ReadUInt32Little(bytes, ref offset);
+            jT808_0X0800.MultiMediaType = JT808BinaryExtensions.ReadByteLittle(bytes, ref offset);
+            jT808_0X0800.MultiMediaCodingFormat = JT808BinaryExtensions.ReadByteLittle(bytes, ref offset);
+            jT808_0X0800.EventItemCoding = JT808BinaryExtensions.ReadByteLittle(bytes, ref offset);
+            jT808_0X0800.ChannelId = JT808BinaryExtensions.ReadByteLittle(bytes, ref offset);
+            readSize = offset;
+            return jT808_0X0800;
+        }
+
+        public int Serialize(IMemoryOwner<byte> memoryOwner, int offset, JT808_0x0800 value)
+        {
+            offset += JT808BinaryExtensions.WriteUInt32Little(memoryOwner, offset, value.MultiMediaId);
+            offset += JT808BinaryExtensions.WriteByteLittle(memoryOwner, offset, value.MultiMediaType);
+            offset += JT808BinaryExtensions.WriteByteLittle(memoryOwner, offset, value.MultiMediaCodingFormat);
+            offset += JT808BinaryExtensions.WriteByteLittle(memoryOwner, offset, value.EventItemCoding);
+            offset += JT808BinaryExtensions.WriteByteLittle(memoryOwner, offset, value.ChannelId);
+            return offset;
+        }
+    }
+}
---
+++ src/JT808.Protocol/MessageBody/JT808_0x0800.cs
@@ -0,0 +1,48 @@
+﻿using JT808.Protocol.Attributes;
+using JT808.Protocol.JT808Formatters.MessageBodyFormatters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JT808.Protocol.MessageBody
+{
+    /// <summary>
+    /// 多媒体事件信息上传
+    /// 0x0800
+    /// </summary>
+    [JT808Formatter(typeof(JT808_0x0800Formatter))]
+    public class JT808_0x0800:JT808Bodies
+    {
+        /// <summary>
+        /// 多媒体数据 ID
+        /// </summary>
+        public uint MultiMediaId { get; set; }
+        /// <summary>
+        /// 多媒体类型
+        /// 0：图像；1：音频；2：视频；
+        /// </summary>
+        public byte MultiMediaType { get; set; }
+        /// <summary>
+        /// 多媒体格式编码 
+        /// 0：JPEG；1：TIF；2：MP3；3：WAV；4：WMV；其他保留
+        /// </summary>
+        public byte MultiMediaCodingFormat { get; set; }
+        /// <summary>
+        /// 事件项编码
+        /// 0：平台下发指令；
+        /// 1：定时动作；
+        /// 2：抢劫报警触发；
+        /// 3：碰撞侧翻报警触发；
+        /// 4：门开拍照；
+        /// 5：门关拍照；
+        /// 6：车门由开变关，时速从＜20 公里到超过 20 公里；
+        /// 7：定距拍照；
+        /// 其他保留
+        /// </summary>
+        public byte EventItemCoding { get; set; }
+        /// <summary>
+        /// 通道 ID
+        /// </summary>
+        public byte ChannelId { get; set; }    
+    }
+}
