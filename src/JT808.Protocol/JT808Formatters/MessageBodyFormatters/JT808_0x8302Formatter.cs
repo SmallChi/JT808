b1577c9971a4dc776bfc6e948aa8aa38c3b49380
SmallChi <564952747@qq.com>
2018-11-06 22:17:03 +0800

    增加0x8302消息

---
+++ src/JT808.Protocol/JT808Formatters/MessageBodyFormatters/JT808_0x8302Formatter.cs
@@ -0,0 +1,41 @@
+﻿using JT808.Protocol.Extensions;
+using JT808.Protocol.MessageBody;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JT808.Protocol.JT808Formatters.MessageBodyFormatters
+{
+    public class JT808_0x8302Formatter : IJT808Formatter<JT808_0x8302>
+    {
+        public JT808_0x8302 Deserialize(ReadOnlySpan<byte> bytes, out int readSize)
+        {
+            int offset = 0;
+            JT808_0x8302 jT808_0X8302 = new JT808_0x8302();
+            jT808_0X8302.Flag = JT808BinaryExtensions.ReadByteLittle(bytes, ref offset);
+            jT808_0X8302.IssueContentLength = JT808BinaryExtensions.ReadByteLittle(bytes, ref offset);
+            jT808_0X8302.Issue = JT808BinaryExtensions.ReadStringLittle(bytes, ref offset, jT808_0X8302.IssueContentLength);
+            jT808_0X8302.AnswerId = JT808BinaryExtensions.ReadByteLittle(bytes, ref offset);
+            jT808_0X8302.AnswerContentLength= JT808BinaryExtensions.ReadUInt16Little(bytes, ref offset);
+            jT808_0X8302.AnswerContent = JT808BinaryExtensions.ReadStringLittle(bytes, ref offset, jT808_0X8302.AnswerContentLength);
+            readSize = offset;
+            return jT808_0X8302;
+        }
+
+        public int Serialize(ref byte[] bytes, int offset, JT808_0x8302 value)
+        {
+            offset += JT808BinaryExtensions.WriteByteLittle(bytes, offset, value.Flag);  
+            // 先计算内容长度（汉字为两个字节）
+            offset += 1;
+            int byteLength1 = JT808BinaryExtensions.WriteStringLittle(bytes, offset, value.Issue);
+            JT808BinaryExtensions.WriteByteLittle(bytes, offset - 1, (byte)byteLength1);
+            offset += byteLength1;
+            offset += JT808BinaryExtensions.WriteByteLittle(bytes, offset, value.AnswerId);
+            offset += 2;
+            int byteLength2 = JT808BinaryExtensions.WriteStringLittle(bytes, offset, value.AnswerContent);
+            JT808BinaryExtensions.WriteByteLittle(bytes, offset - 2, (byte)byteLength2);
+            offset += byteLength2;
+            return offset;
+        }
+    }
+}
---
+++ src/JT808.Protocol/MessageBody/JT808_0x8302.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JT808.Protocol.MessageBody
+{
+    /// <summary>
+    /// 提问下发
+    /// 0x8302
+    /// </summary>
+
+    public class JT808_0x8302:JT808Bodies
+    {
+        /// <summary>
+        /// 标志
+        /// 提问下发标志位定义
+        /// </summary>
+        public byte Flag { get; set; }
+        /// <summary>
+        /// 问题内容长度
+        /// </summary>
+        public byte IssueContentLength { get; set; }
+        /// <summary>
+        /// 问题
+        /// 问题文本，经 GBK 编码，长度为 N
+        /// </summary>
+        public string Issue { get; set; }
+        /// <summary>
+        /// 答案 ID
+        /// </summary>
+        public byte AnswerId { get; set; }
+        /// <summary>
+        /// 答案内容长度
+        /// 答案内容字段字节长度
+        /// </summary>
+        public ushort AnswerContentLength { get; set; }
+        /// <summary>
+        /// 答案内容 
+        /// 答案内容，经 GBK 编码
+        /// </summary>
+        public string AnswerContent { get; set; }
+    }
+}
