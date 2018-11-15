89ce7d2a42f8133869c79e09c13b303ad50179b3
SmallChi <564952747@qq.com>
2018-11-15 18:48:03 +0800

    1.增加长度不够枚举错误 2.修复时间扩展为5位的bug 3.增加消息单元测试

--- src/JT808.Protocol/JT808Formatters/MessageBodyFormatters/JT808_0x0705Formatter.cs
+++ src/JT808.Protocol/JT808Formatters/MessageBodyFormatters/JT808_0x0705Formatter.cs
@@ -1,4 +1,5 @@
-﻿using JT808.Protocol.Extensions;
+﻿using JT808.Protocol.Exceptions;
+using JT808.Protocol.Extensions;
 using JT808.Protocol.MessageBody;
 using System;
 using System.Buffers;
@@ -20,7 +21,15 @@ namespace JT808.Protocol.JT808Formatters.MessageBodyFormatters
             {
                 JT808Properties.JT808CanProperty jT808CanProperty = new JT808Properties.JT808CanProperty();
                 jT808CanProperty.CanId = JT808BinaryExtensions.ReadBytesLittle(bytes, ref offset, 4);
+                if (jT808CanProperty.CanId.Length != 4)
+                {
+                    throw new JT808Exception(Enums.JT808ErrorCode.NotEnoughLength, $"{nameof(jT808CanProperty.CanId)}->4");
+                }
                 jT808CanProperty.CanData = JT808BinaryExtensions.ReadBytesLittle(bytes, ref offset, 8);
+                if (jT808CanProperty.CanData.Length != 8)
+                {
+                    throw new JT808Exception(Enums.JT808ErrorCode.NotEnoughLength, $"{nameof(jT808CanProperty.CanData)}->8");
+                }
                 jT808_0X0705.CanItems.Add(jT808CanProperty);
             }
             readSize = offset;
@@ -35,7 +44,15 @@ namespace JT808.Protocol.JT808Formatters.MessageBodyFormatters
                 offset += JT808BinaryExtensions.WriteDateTime5Little(bytes, offset, value.FirstCanReceiveTime);
                 foreach(var item in value.CanItems)
                 {
+                    if (item.CanId.Length != 4)
+                    {
+                        throw new JT808Exception(Enums.JT808ErrorCode.NotEnoughLength, $"{nameof(item.CanId)}->4");
+                    }
                     offset += JT808BinaryExtensions.WriteBytesLittle(bytes, offset, item.CanId);
+                    if (item.CanData.Length != 8)
+                    {
+                        throw new JT808Exception(Enums.JT808ErrorCode.NotEnoughLength, $"{nameof(item.CanData)}->8");
+                    }
                     offset += JT808BinaryExtensions.WriteBytesLittle(bytes, offset, item.CanData);
                 }
             }
