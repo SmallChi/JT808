bfcde528ca7346398bbe91520c6eec5af2e9efcb
SmallChi <564952747@qq.com>
2018-12-05 18:59:44 +0800

    脑子不够用

--- src/JT808.Protocol/MessageBody/JT808_0x0200.cs
+++ src/JT808.Protocol/MessageBody/JT808_0x0200.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using JT808.Protocol.JT808Formatters.MessageBodyFormatters;
-using JT808.Protocol.MessageBody;
 using JT808.Protocol.Attributes;
-using JT808.Protocol.Enums;
 
 namespace JT808.Protocol.MessageBody
 {
@@ -35,7 +33,6 @@ namespace JT808.Protocol.MessageBody
         /// 高程
         /// 海拔高度，单位为米（m）
         /// </summary>
-  
         public ushort Altitude { get; set; }
         /// <summary>
         /// 速度 1/10km/h
@@ -53,5 +50,22 @@ namespace JT808.Protocol.MessageBody
         /// 位置附加信息
         /// </summary>
         public IDictionary<byte, JT808_0x0200_BodyBase> JT808LocationAttachData { get; set; }
+        /// <summary>
+        /// 自定义位置附加信息
+        /// 场景：
+        /// 一个设备厂商对应多个设备类型，不同设备类型可能存在相同的自定义位置附加信息Id，导致自定义附加信息Id冲突，无法解析。
+        /// 
+        /// 解决方式1：
+        /// a.根据不同的设备类型和相同的自定义附加信息Id组合(DeviceType_AttachId)形如:ab_0xD2、cd_0xD2;
+        /// b.根据设备上传的自定义附加信息Id(0xD2),通过一把梭的方式调用对应自定义附加信息Id(0xD2)的所有解析器;
+        /// c.根据平台录入的设备类型,通过终端号关联设备类型，查找出仅有的一个解析器解析的数据;
+        /// 缺点：依赖平台录入的设备类型
+        /// 一般来说：开发先解析好对应设备的协议，这时候是知道对应的设备类型，可以先死在程序里，然后录入到对应的平台中。
+        /// 
+        /// 解决方式2：
+        /// 根据不同的设备对应不同的端口，通过端口解析不同设备;
+        /// 缺点：需要维护端口和设备之间的关联及对应的解析服务
+        /// </summary>
+        public IDictionary<string, JT808_0x0200_CustomBodyBase> JT808CustomLocationAttachData { get; set; }
     }
 }
