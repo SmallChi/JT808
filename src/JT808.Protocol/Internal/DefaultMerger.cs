b4e6bf9733790428ee9ee6992d9f0ee5b85288e6
yedajiang44 <602830483@qq.com>
2023-04-20 00:02:31 +0800

    自动合并分包功能兼容乱序数据

--- src/JT808.Protocol/Internal/DefaultMerger.cs
+++ src/JT808.Protocol/Internal/DefaultMerger.cs
@@ -18,29 +18,35 @@ namespace JT808.Protocol.Internal
         /// <inheritdoc/>
         public bool TryMerge(JT808Header header, byte[] data, IJT808Config config, out JT808Bodies body)
         {
+            // TODO: 添加SplitPackages缓存超时，达到阈值时移除该项缓存
             body = null;
-            if (header.PackageIndex == header.PackgeCount)
-            {
-                if (SplitPackages.TryRemove(header.TerminalPhoneNo, out var item) && item.TryRemove(header.MsgId, out var packages))
-                {
-                    var mateData = packages.OrderBy(x => x.index).SelectMany(x => x.data).Concat(data).ToArray();
 
-                    byte[] buffer = JT808ArrayPool.Rent(mateData.Length);
-                    try
+            if (SplitPackages.TryGetValue(header.TerminalPhoneNo, out var item) && item.TryGetValue(header.MsgId, out var packages))
+            {
+                packages.Add((header.PackageIndex, data));
+                if (packages.Count != header.PackgeCount)
+                {
+                    return false;
+                }
+                item.TryRemove(header.MsgId, out _);
+                SplitPackages.TryRemove(header.TerminalPhoneNo, out _);
+
+                var mateData = packages.OrderBy(x => x.index).SelectMany(x => x.data).Concat(data).ToArray();
+
+                byte[] buffer = JT808ArrayPool.Rent(mateData.Length);
+                try
+                {
+                    var reader = new JT808MessagePackReader(mateData, (Enums.JT808Version)header.ProtocolVersion);
+                    if (config.MsgIdFactory.TryGetValue(header.MsgId, out var value) && value is JT808Bodies instance)
                     {
-                        var reader = new JT808MessagePackReader(mateData, (Enums.JT808Version)header.ProtocolVersion);
-                        if (config.MsgIdFactory.TryGetValue(header.MsgId, out var value) && value is JT808Bodies instance)
-                        {
-                            body = instance.DeserializeExt<JT808Bodies>(ref reader, config);
-                            return true;
-                        }
-                    }
-                    finally
-                    {
-                        JT808ArrayPool.Return(buffer);
+                        body = instance.DeserializeExt<JT808Bodies>(ref reader, config);
+                        return true;
                     }
                 }
-                return default;
+                finally
+                {
+                    JT808ArrayPool.Return(buffer);
+                }
             }
             else
             {
